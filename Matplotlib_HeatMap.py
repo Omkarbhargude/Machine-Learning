7127ec91db18b2a1bdeabc18b93b8293f534cdab
Omkar Mahadev Bhargude <omkarbhargude29@gmail.com>
2026-03-03 22:45:17 +0530

    Add files via upload

---
+++ Matplotlib_HeatMap.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def main():
+    # used in co-relation matrix 
+    # used to display Feature correlation
+    dobj = pd.DataFrame({
+            "A" : [1,2,3],
+            "B" : [4,5,6],
+            "C" : [7,8,9]
+        })
+
+    print(dobj)
+    
+    sns.heatmap(dobj.corr(), annot=True)
+    
+    plt.show()
+
+if __name__ == "__main__":
+    main()
