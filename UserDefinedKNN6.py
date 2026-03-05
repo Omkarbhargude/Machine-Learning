92359472ee39ae5cfdaa5133a6d583906c7911e4
Omkar Mahadev Bhargude <omkarbhargude29@gmail.com>
2026-03-05 23:29:25 +0530

    Add files via upload

---
+++ UserDefinedKNN6.py
@@ -0,0 +1,99 @@
+#  [A,B,C,D]
+# X[1,2,3,5]
+# Y[2,3,1,6]
+#  [R,R,B,B]
+
+# Predict(3,3) -> ?
+
+import numpy as np
+import math
+
+def EucDistance(P1,P2):
+    Ans = math.sqrt((P1["X"]- P2["X"]) ** 2 + (P1["Y"] - P2["Y"]) ** 2)
+    return Ans
+
+
+def MaravellousKNeighborsClassifier():
+    Border = "-"*40
+
+    data = [
+                {"point" : "A", "X" : 1, "Y" : 2, "label" : "Red"},
+                {"point" : "B", "X" : 2, "Y" : 3, "label" : "Red"},
+                {"point" : "C", "X" : 3, "Y" : 1, "label" : "Blue"},
+                {"point" : "D", "X" : 5, "Y" : 6, "label" : "Blue"}
+            ]
+
+    print(Border)
+    print("Marvellous user defined KNN")
+    print(Border)
+
+    print(Border)
+    print("Training data set")
+    print(Border)
+
+    for i in data:
+        print(i)
+
+    print(Border)
+
+    new_point = {"X" : 3, "Y" : 3}
+
+    # calculate all distances 
+    for d in data:
+        # dict here d creates new key in data list of dictionary
+        d["distance"] = EucDistance(d,new_point)
+    
+    print(Border)
+    print("Calculated distances are : ")
+    print(Border)
+
+    for d in data:
+        print(d)
+
+    # sorting gives first 3 data to predict new_point
+    # this will sort on distance key
+    sorted_data = sorted(data, key=lambda item : item["distance"])
+
+    print(Border)
+    print("Sorted data is : ")
+    print(Border)
+
+    for d in sorted_data:
+        print(d)
+
+    k = 3
+    nearest = sorted_data[:k]
+
+    print(Border)
+    print("Nearest 3 elements are : ")
+    print(Border)
+
+    for d in nearest:
+        print(d)
+    
+    # voting to decide what is k
+    votes = {}
+
+    for neighbour in nearest:
+        label = neighbour["label"]
+        votes[label] = votes.get(label,0) + 1
+
+    print(Border)
+    print("Voting result is : ")
+    print(Border)
+
+    for d in votes:
+        print("Name : ",d, "Number of votes : ",votes[d])
+
+    print(Border)
+    
+    predicted_class = max(votes, key=votes.get)
+
+    print("Predicted class of (3,3) is : ",predicted_class)
+    
+def main():
+
+    MaravellousKNeighborsClassifier()
+
+if __name__ == "__main__":
+    main()
